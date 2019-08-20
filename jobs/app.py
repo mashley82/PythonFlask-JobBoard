4a2fda2182307fbd23b8280f4c052a98761893a1
Micheal Ashley <micheal.ashley@macefusion.com>
2019-08-20 10:18:41 -0400

    Loads Jobs from sqlite to page using macros

--- jobs/app.py
+++ jobs/app.py
@@ -5,7 +5,6 @@ PATH = "db/jobs.sqlite"
 
 app = Flask(__name__)
 
-
 def open_connection():
     connection = getattr(g, '_connection', None)
     if connection is None:
@@ -13,7 +12,6 @@ def open_connection():
         g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
-
 
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
@@ -25,15 +23,14 @@ def execute_sql(sql, values=(), commit=False, single=False):
     cursor.close()
     return results
 
-
 @app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
     if connection is not None:
         connection.close()
 
-
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    return render_template('index.html')
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
+    return render_template('index.html', jobs=jobs)
